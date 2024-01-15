65bae41f88622fc959c5e310be6db7e7201be0a0
unknown <tateraog30@gmail.com>
2024-01-15 21:07:09 +0530

    data added

--- src/main/java/Tutorial1/Demo1.java
+++ src/main/java/Tutorial1/Demo1.java
@@ -1,7 +1,12 @@
 package Tutorial1;
 
 public class Demo1 {
-public static void main(String[] args) {
-	System.out.println("Demo");
-}
+	public static void main(String[] args)
+	{
+		System.out.println("Demo");
+	}
+	public void m1()
+	{
+		System.out.println("Newly added method");
+	}
 }
